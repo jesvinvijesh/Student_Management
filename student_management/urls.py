acc613e1f5068ed7baa2b34b75a0ecb56e7b2fa7
Jesvin Vijesh <msjjesvin@gmail.com>
2020-06-13 11:40:49 +0530

    initial version

--- student_management/urls.py
+++ student_management/urls.py
@@ -18,11 +18,10 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('students/', include('students.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
